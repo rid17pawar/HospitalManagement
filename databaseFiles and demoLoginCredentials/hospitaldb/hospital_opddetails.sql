08f784c886c113ff5b0b2ae80fe181eea7e79320
Riddhi Pawar <47048717+rid17pawar@users.noreply.github.com>
2021-12-04 21:55:46 +0530

    uploading new database files

---
+++ databaseFiles and demoLoginCredentials/hospitaldb/hospital_opddetails.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
+--
+-- Host: localhost    Database: hospital
+-- ------------------------------------------------------
+-- Server version	8.0.18
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `opddetails`
+--
+
+DROP TABLE IF EXISTS `opddetails`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `opddetails` (
+  `opdid` varchar(255) NOT NULL,
+  `symptoms` varchar(255) DEFAULT NULL,
+  `diagnosis` varchar(255) DEFAULT NULL,
+  `medicinesDose` varchar(255) DEFAULT NULL,
+  `dos` varchar(255) DEFAULT NULL,
+  `donts` varchar(255) DEFAULT NULL,
+  `investigations` varchar(255) DEFAULT NULL,
+  `followupDate` varchar(255) DEFAULT NULL,
+  `fees` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`opdid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `opddetails`
+--
+
+LOCK TABLES `opddetails` WRITE;
+/*!40000 ALTER TABLE `opddetails` DISABLE KEYS */;
+INSERT INTO `opddetails` VALUES ('1','#headache #bodypain','weakness','#crocin@2 #neutrolin-B@3','#drink warm water','#junk food','none','2020-06-20','200'),('7','#abdominal pain #fever #nausea','typhoid','#crocin500@2  #meftal spas@2 #ofloxine500@1','#rest #eat dal-chaval #warm water','#oily food #cold drinks','blood test','2020-06-26','500');
+/*!40000 ALTER TABLE `opddetails` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-12-04 21:52:43
